b59f30f5a79fa4577e8c538b307eafd1be224eeb
Sören Uhrbach <s.uhrbach@aditus.de>
2022-07-08 13:38:11 +0200

    fixed guids

--- src/ADITUS.CodeChallenge.API/Services/EventService.cs
+++ src/ADITUS.CodeChallenge.API/Services/EventService.cs
@@ -12,7 +12,7 @@ namespace ADITUS.CodeChallenge.API.Services
       {
         new Event
         {
-          Id = Guid.NewGuid(), // TODO: fixed guid
+          Id = Guid.Parse("7c63631c-18d4-4395-9c1e-886554265eb0"), // TODO: fixed guid
           Year = 2019,
           Name = "ADITUS Code Challenge 2019",
           StartDate = new DateTime(2019, 1, 1),
@@ -21,7 +21,7 @@ namespace ADITUS.CodeChallenge.API.Services
         },
         new Event
         {
-          Id = Guid.NewGuid(),
+          Id = Guid.Parse("751fd775-2c8e-48e0-955c-2144008e984a"),
           Year = 2020,
           Name = "ADITUS Code Challenge 2020",
           StartDate = new DateTime(2020, 1, 1),
@@ -30,7 +30,7 @@ namespace ADITUS.CodeChallenge.API.Services
         },
         new Event
         {
-          Id = Guid.NewGuid(),
+          Id = Guid.Parse("974098e0-9b3f-41d5-80c2-551600ad204a"),
           Year = 2021,
           Name = "ADITUS Code Challenge 2021",
           StartDate = new DateTime(2021, 1, 1),
@@ -39,7 +39,7 @@ namespace ADITUS.CodeChallenge.API.Services
         },
         new Event
         {
-          Id = Guid.NewGuid(),
+          Id = Guid.Parse("28669572-2b9a-4b2c-ad7e-6434ea8ab761"),
           Year = 2022,
           Name = "ADITUS Code Challenge 2022",
           StartDate = new DateTime(2022, 1, 1),
@@ -48,7 +48,7 @@ namespace ADITUS.CodeChallenge.API.Services
         },
         new Event
         {
-          Id = Guid.NewGuid(),
+          Id = Guid.Parse("3a17b294-8716-448c-94db-ebf9bf53f1ce"),
           Year = 2023,
           Name = "ADITUS Code Challenge 2023",
           StartDate = new DateTime(2023, 1, 1),
